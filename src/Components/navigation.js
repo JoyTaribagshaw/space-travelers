1cb9d21aad66eb8d30645f4608236255aedcbcfd
sheezu900 <shahzamanlive2@gmail.com>
2023-08-29 23:54:34 +0500

    change color

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -3,10 +3,10 @@ import img from '../assets/planet.png';
 
 function Navigation() {
   return (
-    <nav className="px-4 py-8 flex justify-between items-center border-b-2">
+    <nav className="px-16 py-8 flex justify-between items-center border-b-2 text-blue-500">
       <div className="flex items-center gap-4">
         <img src={img} alt="" height={50} width={50} />
-        <h1>Space Travelers Hub</h1>
+        <h1 className="text-black text-lg">Space Travelers Hub</h1>
       </div>
       <ul className="flex gap-4 border-b-slate-400">
         <li><NavLink to="/">Rockets</NavLink></li>
