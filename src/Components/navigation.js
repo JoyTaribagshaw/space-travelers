8b09ede000e9baa8f302aa22b6f72492654ae5aa
sheezu900 <shahzamanlive2@gmail.com>
2023-08-29 23:49:27 +0500

    header complete

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -1,9 +1,14 @@
 import { NavLink } from 'react-router-dom';
+import img from '../assets/planet.png';
 
 function Navigation() {
   return (
-    <nav className="px-4 py-8">
-      <ul>
+    <nav className="px-4 py-8 flex justify-between items-center border-b-2">
+      <div className="flex items-center gap-4">
+        <img src={img} alt="" height={50} width={50} />
+        <h1>Space Travelers Hub</h1>
+      </div>
+      <ul className="flex gap-4 border-b-slate-400">
         <li><NavLink to="/">Rockets</NavLink></li>
         <li><NavLink to="/missions">Missions</NavLink></li>
         <li><NavLink to="/myprofile">My profile</NavLink></li>
