1ace4905b6cc448304c84eb38b30568c8ae2ca42
sheezu900 <shahzamanlive2@gmail.com>
2023-08-30 15:59:39 +0500

    remove comments please add again

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -10,15 +10,12 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
-    // This reducer will be called when the `missions` action is dispatched
     setRockets(state, action) {
       state.rockets = action.payload;
     },
-    // This reducer will be called when the `loading` action is dispatched
     setLoading(state, action) {
       state.loading = action.payload;
     },
-    // This reducer will be called when the `error` action is dispatched
     setError(state, action) {
       state.error = action.payload;
     },
