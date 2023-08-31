3bbf773a4c26038e77830cd6c5533969aa42bc11
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-31 19:44:04 +0100

    render list of all reserved rockets

--- src/Components/myprofile.js
+++ src/Components/myprofile.js
@@ -1,7 +1,23 @@
-function Myprofile() {
-  return (
-    <h1>My Profile Component</h1>
-  );
-}
+import { useSelector } from 'react-redux';
 
-export default Myprofile;
+const Profile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+
+  return (
+    <div className="profile">
+      <div className="my-profile">
+        <div className="my-missions">
+          <h1 className="heading">My Rockets</h1>
+          <ul>
+            {rockets.filter((rocket) => rocket.reserved)
+              .map((rocket) => (
+                <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
