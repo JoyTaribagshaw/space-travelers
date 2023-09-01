e2274c3e6b90dbff90959b257dfbe3983eb426e5
sheezu900 <shahzamanlive2@gmail.com>
2023-09-01 14:25:29 +0500

    ,

--- src/tests/missions.test.js
+++ src/tests/missions.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import Missions from '../components/missions';
+import Missions from '../components/missions.js';
 
 test('Missions component rendering test', () => {
   const tree = render(
